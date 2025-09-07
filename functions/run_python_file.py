b41d6e196d355b707c5964f9234075d555d820fc
RustemGaliulin <123078475+RustemGaliulin@users.noreply.github.com>
2025-09-07 16:09:03 -0700

    handled the LLM's function calling

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -1,6 +1,6 @@
 import os
 import subprocess
-
+from google.genai import types
 
 def run_python_file(working_directory, file_path, args=None):
     abs_working_dir = os.path.abspath(working_directory)
@@ -34,3 +34,24 @@ def run_python_file(working_directory, file_path, args=None):
         return "\n".join(output) if output else "No output produced."
     except Exception as e:
         return f"Error: executing Python file: {e}"
+    
+schema_run_python_file = types.FunctionDeclaration(
+    name="run_python_file",
+    description="Runs a ptyhon file with the python3 interpreter. Accepts additional CLI args as an optional array",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The file to run, relative to the working directory.",
+            ),
+            "args": types.Schema(
+                type=types.Type.ARRAY,
+                description="An optional array of strings to be used as the CLI args for the python file",
+                items = types.Schema(
+                    type = types.Type.STRING
+                )
+            ),
+        },
+    ),
+)
