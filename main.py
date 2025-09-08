455a9467e73a035e19dcbcbbe0d676d60402ae25
RustemGaliulin <123078475+RustemGaliulin@users.noreply.github.com>
2025-09-07 18:20:22 -0700

    adjusted the request response model giving LLM tools to choose among the functions available

--- main.py
+++ main.py
@@ -55,28 +55,41 @@ def main():
         tools=[available_functions], system_instruction=system_prompt
     )
 
-    response = client.models.generate_content(
-        model='gemini-2.0-flash-001',
-        contents=messages,
-        config=config
-    )
+    max_iters = 20
 
-    if response is None or response.usage_metadata is None:
-        print("Response is malformed")
-        return
+    for i in range(max_iters):
+        response = client.models.generate_content(
+            model='gemini-2.0-flash-001',
+            contents=messages,
+            config=config
+        )
 
-    if verbose_flag:
-        print(f"User prompt: {prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(
-            f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        if response is None or response.usage_metadata is None:
+            print("Response is malformed")
+            return
 
-    if response.function_calls:
-        for function_call_part in response.function_calls:
-            result = call_function(function_call_part)
-            print(result)
-    else:
-        print(response.text)
+        if verbose_flag:
+            print(f"User prompt: {prompt}")
+            print(
+                f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(
+                f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+        if response.candidates:
+            for candidate in response.candidates:
+                if candidate is None or candidate.content is None:
+                    continue
+                messages.append(candidate.content)
+
+                
+        if response.function_calls:
+            for function_call_part in response.function_calls:
+                result = call_function(function_call_part, verbose_flag)
+                messages.append(result)
+                
+        else:
+            print(response.text)
+            return
 
 
 main()
